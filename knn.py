64e33fb5e00e15f95b86924689a4a3ad2550dd66
Jorge Guilherme <83836771+Jorge-Guilherme@users.noreply.github.com>
2023-08-06 15:35:19 -0300

    knn.py

--- knn.py
+++ knn.py
@@ -1,7 +1,6 @@
 # Importando as bibliotecas
 
 import numpy as np
-import matplotlib.pyplot as plt
 from iris import rawIrisData as database
 import iris_train as trainbase
 
@@ -16,6 +15,7 @@ def points_distance(p1, p2):
 
 # Função para o algoritmo KNN
 def knn(vet_train, vet_test, k):
+    spected_class = 0
     label = []
     for w in vet_test:
         d = []
@@ -35,57 +35,25 @@ def knn(vet_train, vet_test, k):
         kn = max(counts, key=counts.get)
         label.append(kn)
 
-    return label
+        if w[-1] == kn: # Calcular acurácia
+            spected_class += 1
+
+        print(f"Classe do Algoritmo: [{int(kn)}] Classe Esperada: [{int(w[-1])}]\n")
+
+    return label, spected_class
 
 # Dividindo os dados do Iris em treino e testes
 vet_train = database
-vet_test = trainbase.data_train_classtwo
+vet_test = trainbase.data_train
 
 k = 3  # Valor de k para o KNN
 
-preview = knn(vet_train, vet_test, k)
+preview, spected_class = knn(vet_train, vet_test, k)
 
 # Calcular acurácia do treinamento
 
-spected_class = 2
-correct = 0
-
 qtd_vet = len(vet_test)
 
-for i in preview:
-    if i == spected_class:
-        correct += 1 
+acuracy = (spected_class * 100) / qtd_vet
 
-acuracy = (correct * 100) / qtd_vet
-
-print(f"Sua taxa de acerto foi de: {acuracy:,.2f}%")
-
-#############################################################################################
-
-# Plotar os pontos de treinamento e de teste em 3D
-x_train = vet_train[:, 0]
-y_train = vet_train[:, 1]
-z_train = vet_train[:, 2]
-
-x_test = vet_test[:, 0]
-y_test = vet_test[:, 1]
-z_test = vet_test[:, 2]
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# Plotar os pontos de treinamento
-ax.scatter(x_train, y_train, z_train, c=z_train, label="Dados de Treino", cmap='viridis', marker='o')
-
-# Plotar o ponto de teste
-ax.scatter(x_test, y_test, z_test, c=preview, label="Dado de Teste", cmap='viridis', marker='x', s=100)
-
-# Definir rótulos dos eixos e título do gráfico
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_title('KNN')
-
-ax.legend()
-
-plt.show()
+print(f"Taxa de Acerto: {acuracy:,.2f}%")
